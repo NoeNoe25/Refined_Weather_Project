555942a2296c312606712c55ddbf08b99e8f99fe
NoeNoe25 <144063126+NoeNoe25@users.noreply.github.com>
2023-12-12 01:52:36 +0630

    Refreshed Temperature

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+function UpdateWeather(response){
+  let updateTemp= document.querySelector("#degree");
+  updateTemp.innerHTML=Math.round(response.data.main.temp);
+}
+function search_city(city){
+  let apiKey="995876feccff84467560f64d2dabfe70";
+  let apiURL=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiURL).then(UpdateWeather);
+}
 function search_handle(event) {
     event.preventDefault();
     let s_bar = document.querySelector("#search_bar");
     let city = document.querySelector("#city");
     city.innerHTML = s_bar.value;
+    search_city(s_bar.value);
   }
   let search = document.querySelector("#search_form");
   search.addEventListener("click", search_handle);
